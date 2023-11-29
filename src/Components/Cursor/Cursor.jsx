f7016d711920af7e8c99edbe4029f6519dc69eae
Rhitam-Banerjee <banerjeerhitam1@gmail.com>
2023-11-29 13:21:14 +0530

    FixedCursorScroll

--- src/Components/Cursor/Cursor.jsx
+++ src/Components/Cursor/Cursor.jsx
@@ -31,7 +31,7 @@ const Cursor = () => {
 
   useEffect(() => {
     controls.start({
-      strokeDasharray: `${scrollProgress},100`,
+      strokeDasharray: `${scrollProgress} 100`,
       strokeDashoffset: `0`,
     });
   }, [scrollProgress, controls]);
@@ -40,7 +40,7 @@ const Cursor = () => {
       <motion.div
         style={{ left: `${x}px`, top: `${y}px` }}
         className={`fixed top-1/2 left-1/2 h-[30px] w-[30px] -translate-x-1/2 -translate-y-1/2 pointer-events-none z-[99] ${
-          changeShape ? "!scale-[2]" : ""
+          changeShape ? "!scale-[2]" : "scale-[1.4]"
         }`}
       >
         <motion.svg
@@ -50,9 +50,7 @@ const Cursor = () => {
           xmlns="http://www.w3.org/2000/svg"
         >
           <motion.circle
-            className={`stroke-primary_transparant ${
-              changeShape ? "fill-primary_transparant_max" : "fill-none"
-            }`}
+            className={`stroke-secondary fill-primary_transparant_max`}
             cx="18"
             cy="18"
             r="15"

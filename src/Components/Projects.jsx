72525581fd9721f7b525004a8e2f9e4f6ea6e17e
Rhitam-Banerjee <banerjeerhitam1@gmail.com>
2023-12-02 20:57:22 +0530

    minorChange

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -63,7 +63,7 @@ const Experience = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} text-center `}>
+        <p className={`${styles.sectionSubText} text-center text-tertiary `}>
           What I have done so far
         </p>
         <h2 className={`${styles.heroHeadText} text-primary text-center`}>
